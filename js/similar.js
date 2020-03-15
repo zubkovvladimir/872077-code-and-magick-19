c74b0646d2e24b91ac3dda2c7785d0e97cbbe6cc
zubkovvladimir <njoy70@mail.ru>
2020-03-15 18:03:12 +0500

    Учебный проект: фильтруй заклинания!

---
+++ js/similar.js
@@ -0,0 +1,73 @@
+'use strict';
+
+(function () {
+  var coatColor;
+  var eyesColor;
+  var wizards = [];
+
+  var getRank = function (wizard) {
+    var rank = 0;
+
+    if (wizard.colorCoat === coatColor) {
+      rank += 2;
+    }
+    if (wizard.colorEyes === eyesColor) {
+      rank += 1;
+    }
+
+    return rank;
+  };
+
+  var namesComparator = function (left, right) {
+    if (left > right) {
+      return 1;
+    } else if (left < right) {
+      return -1;
+    } else {
+      return 0;
+    }
+  };
+
+  var updateWizards = function () {
+    window.render.wizard(wizards.sort(function (left, right) {
+      var rankDiff = getRank(right) - getRank(left);
+      if (rankDiff === 0) {
+        rankDiff = namesComparator(left.name, right.name);
+      }
+      return rankDiff;
+    }));
+  };
+
+  window.wizard.coatChangeHandler = function (color) {
+    coatColor = color;
+    updateWizards();
+  };
+
+  window.wizard.eyesChangeHandler = function (color) {
+    eyesColor = color;
+    updateWizards();
+  };
+
+  var successHandler = function (data) {
+    wizards = data;
+    updateWizards();
+  };
+
+  var errorHandler = function (errorMessage) {
+    var node = document.createElement('div');
+    node.style = 'z-index: 100; margin: 0 auto; text-align: center; background-color: red;';
+    node.style.position = 'absolute';
+    node.style.left = 0;
+    node.style.right = 0;
+    node.style.fontSize = '30px';
+
+    node.textContent = errorMessage;
+    document.body.insertAdjacentElement('afterbegin', node);
+  };
+
+  window.backend.load(successHandler, errorHandler);
+
+  window.similar = {
+    updateWizards: updateWizards
+  };
+})();
